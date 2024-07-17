7166b793f493a0c9e25f4de85c53f09d349e716a
Danil Biryukov <danilbiryukov04@gmail.com>
2024-07-17 22:37:19 +0300

    Начало проекта

---
+++ src/app/pages/pic-create/pic-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pic-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './pic-create.component.html',
+  styleUrl: './pic-create.component.scss'
+})
+export class PicCreateComponent {
+
+}
---
+++ src/app/pages/pic-detail/pic-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pic-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './pic-detail.component.html',
+  styleUrl: './pic-detail.component.scss'
+})
+export class PicDetailComponent {
+
+}
